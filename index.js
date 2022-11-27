afb2a6f72e75b9887b9a89517cba26262c41a377
siddhi mittal <mittal@pdx.edu>
2022-11-26 20:22:21 -0800

    formatting issues resolved

--- index.js
+++ index.js
@@ -13,25 +13,25 @@ const handleSubmit = (event) => {
         date == "" &&
         message == ""
     ){
-        msgforerror.style = "display:block";
-        msgforclear.style = "display:none";
+        msgforerror.style.display = "block";
+        msgforclear.style.display = "none";
     } else if (name == "") {
         alert("Please fill name and other missing details")
     } else if (email == "") {
         alert("please fill the email and other missing details")
-        msgforerror.style = "display:none";
-        msgforclear.style = "display:none";
+        msgforerror.style.display = "none";
+        msgforclear.style.display = "none";
     } else if (message == "") {
         alert("please fill the message and other missing details")
-        msgforerror.style = "display:none";
-        msgforclear.style = "display:none";
+        msgforerror.style.display = "none";
+        msgforclear.style.display = "none";
     } else if (date == "") {
         alert("please fill the date and other missing details")
-        msgforerror.style = "display:none";
-        msgforclear.style = "display:none";
+        msgforerror.style.display = "none";
+        msgforclear.style.display = "none";
     } else {
         msgforerror.style.display = "none";
-        msgforclear.style = "display:block";
+        msgforclear.style.display = "block";
         form.elements.name.value = "";
         form.elements.email.value = "";
         form.elements.message.value = "";
